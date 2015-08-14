39375bf668266b404e8d924600621a97b2cce482
AlbChi <albchi@gmail.com>
2015-08-14 08:42:34 -0700

    Added GPS HW detection at startup

--- app/src/main/java/com/example/aceral/gmap/MapsActivity.java
+++ app/src/main/java/com/example/aceral/gmap/MapsActivity.java
@@ -1,9 +1,19 @@
 package com.example.aceral.gmap;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,12 +27,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
 
+    private TextView mLocationView;
+    private LocationManager mManager; // gps
+    private Location mCurrentLocation; // gps
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // check to see if GPS is even in the damn tablet/phone
+
+        PackageManager packageManager = this.getPackageManager();
+        boolean hasGPS = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+        if(hasGPS)
+            Toast.makeText(this, "GPS detected!", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "GPS NOT detected!", Toast.LENGTH_SHORT).show();
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); // gps
+        // mLocationView = new TextView(this);
+        mLocationView = (TextView)findViewById(R.id.mLocationView);
+        mLocationView.setText("Hello from inside the app");
     }
 
     @Override
@@ -54,9 +82,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(disneyMagicKingdom)
                 .title("Kingdom"));
         bounds.include(new LatLng(28.418971, -81.581436));
+        map.setMyLocationEnabled(true);
 
-
-        map.moveCamera(CameraUpdateFactory.newLatLng(disneySevenLagoon));
+        // map.moveCamera(CameraUpdateFactory.newLatLng(disneySevenLagoon)); // disneySevenLagoon is type LatLng
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude())));
         // CameraUpdate cU = CameraUpdateFactory.newLatLngBounds(bounds.build(), 100, 100, 50);
 
         //map.moveCamera(cU);
@@ -77,6 +106,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
        Log.d("XAC", "Click!");
 
    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!mManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            //Ask the user to enable GPS
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Manager");
+            builder.setMessage(
+                    "We would like to use your location, "
+                            + "but GPS is currently disabled.\n"
+                            + "Would you like to change these settings "
+                            + "now?");
+            builder.setPositiveButton("Yes",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog,
+                                            int which) {
+                            //Launch settings, allowing user to change
+                            Intent i = new Intent(Settings
+                                    .ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(i);
+                        }
+                    });
+            builder.setNegativeButton("No",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog,
+                                            int which) {
+                            //No location service, no Activity
+                            finish();
+                        }
+                    });
+            builder.create().show();
+        }
+        //Get a cached location, if it exists
+        mCurrentLocation = mManager.getLastKnownLocation(
+                LocationManager.GPS_PROVIDER);
+        updateDisplay();
+        //Register for updates
+        int minTime = 5000;
+        float minDistance = 0;
+        mManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                mListener);
+    } // onResume
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mManager.removeUpdates(mListener);
+    }
+
+    private void updateDisplay() {
+        if(mCurrentLocation == null) {
+            mLocationView.setText("Determining Your Location...");
+        } else {
+            mLocationView.setText(
+                    String.format("Your Location:\n%.2f, %.2f",
+                            mCurrentLocation.getLatitude(),
+                            mCurrentLocation.getLongitude()));
+        }
+        // Map tmp;
+        // tmp = Map.getMapAsync();
+        // map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude())));
+
+    } // updateDisplay
+
+    private LocationListener mListener = new LocationListener() {
+        //New location event
+        @Override
+        public void onLocationChanged(Location location) {
+            mCurrentLocation = location;
+            updateDisplay();
+        }
+
+        //The requested provider was disabled in settings
+        @Override
+        public void onProviderDisabled(String provider) { }
+
+        //The requested provider was enabled in settings
+        @Override
+        public void onProviderEnabled(String provider) { }
+
+        //Availability changes in the requested provider
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) { }
+
+    };
 
 
     // Add a marker in Sydney, Australia, and move the camera.
